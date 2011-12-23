2324a2edd8fdc7b29c54146d2c44c3b84fa51bed
Grgur Grisogono <grguru@gmail.com>
2011-12-22 19:28:14 -0700

    Added Socket.io debug buttons

--- node/public/Othello/app/view/SocketDebug.js
+++ node/public/Othello/app/view/SocketDebug.js
@@ -6,16 +6,81 @@ Ext.define('Othello.view.SocketDebug', {
     config : {
         title   : 'Socket Debug',
         padding : 10,
-        layout  : 'vbox'
+        layout  : {
+            type: 'vbox',
+            align: 'middle',
+            pack: 'center'
+        },
+
+        defaults: {
+            xtype: 'button',
+            margin: 10,
+            width: 480
+        }
     },
 
     initialize : function() {
         this.setItems([
             {
-                xtype: 'button',
-                text: 'Some socket.io action',
+                text: 'Send Hi',
                 handler: function() {
-                    console.log('Pushed');
+                    socket.emit('hi', 'Hello mofo');
+                }
+            },
+
+            {
+                text: 'Register as player Bruce Willis',
+                ui  : 'confirm',
+                handler: function() {
+                    socket.emit('registerPlayer', {
+                        name    : 'Bruce Willis',
+                        email   : 'bruce@willis.us'
+                    });
+                }
+            },
+
+            {
+                text: 'Who am I?',
+                handler: function() {
+                    socket.emit('whoami');
+                }
+            },
+
+            {
+                text: 'Start game',
+                handler: function() {
+                    socket.emit('startNewSession', {});
+                }
+            },
+
+            {
+                text: 'Continue game',
+                handler: function() {
+                    socket.emit('continueSession', {});
+                }
+            },
+
+            {
+                text: 'Tap position 5,5 as current player (white)',
+                ui  : 'action',
+                handler: function() {
+                    socket.emit('tapBoard', {x:5,y:5});
+                }
+            },
+
+            {
+                text: 'Tap position 5,5 as opponent (black)',
+                ui  : 'action',
+                handler: function() {
+                    socket.emit('tapBoard', {x:5,y:5, player:0});
+                }
+            },
+
+            {
+                text: 'Be a bastard that deserves getting his ass kicked',
+                ui  : 'decline',
+                handler: function() {
+                    socket.emit('yell', 'Fucking fucker fuck fuck');
                 }
             }
         ]);
